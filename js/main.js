221e7035d1369c164df25549b30c102667032bf0
Ron-Ancheta <113373294+Ron-Ancheta@users.noreply.github.com>
2024-10-01 21:21:30 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 // Enter your code below.
+document.addEventListener('DOMContentLoaded', function() {
+    const showResourcesButton = document.getElementById('show-resources');
+    const resourcesDiv = document.querySelector('.javascript-resources');
+
+    showResourcesButton.addEventListener('click', function() {
+        resourcesDiv.classList.toggle('d-none');  
+    });
+
+    resourcesDiv.addEventListener('mouseover', function(event) {
+        if (event.target.classList.contains('list-group-item')) {
+            event.target.classList.add('fw-bold');
+        }
+    });
+
+    resourcesDiv.addEventListener('mouseout', function(event) {
+        if (event.target.classList.contains('list-group-item')) {
+            event.target.classList.remove('fw-bold');
+        }
+    });
+
+    resourcesDiv.addEventListener('click', function(event) {
+        if (event.target.classList.contains('list-group-item')) {
+            event.target.classList.add('fst-italic');
+        }
+    });
+});
+
